2a721c05234499c7d4262b3ff928e03d33e280ea
Matthew Gerring <gerring.matthew@gmail.com>
2014-08-01 12:09:21 -0700

    archives styling

--- wp-content/themes/digitalcitizen/content.php
+++ wp-content/themes/digitalcitizen/content.php
@@ -20,21 +20,9 @@
 			</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content">
-		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'digitalcitizen' ) ); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'digitalcitizen' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-	<?php endif; ?>
 
 	<footer class="entry-footer">
 		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
