11502415f9aa0e2b06524240502093d63e88b02b
Matthew Gerring <matthewgerring@riseup.net>
2014-10-01 11:02:37 -0700

    moved post footer functions to social bar

--- wp-content/themes/digitalcitizen/content.php
+++ wp-content/themes/digitalcitizen/content.php
@@ -24,29 +24,4 @@
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'digitalcitizen' ) );
-				if ( $categories_list && digitalcitizen_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', 'digitalcitizen' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'digitalcitizen' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tags-links">
-				<?php printf( __( 'Tagged %1$s', 'digitalcitizen' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
-
-		<?php edit_post_link( __( 'Edit', 'digitalcitizen' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
--- wp-content/themes/digitalcitizen/socialbar.php
+++ wp-content/themes/digitalcitizen/socialbar.php
@@ -2,4 +2,12 @@
 	<span class="share-text">Share this article:</span>
 	<a href="" class="twitter-share icon-twitter">Twitter</a>
 	<a href="" class="facebook-share icon-facebook">Facebook</a>
+	<?php 
+	printf(
+		__( '<a href="%1$s" rel="bookmark">Permalink</a>.', 'digitalcitizen' ),
+		get_permalink()
+	);
+	?>
+	<?php edit_post_link( __( 'Edit', 'digitalcitizen' ), '<span class="edit-link">', '</span>' ); ?>
+
 </aside>
