9402b4c677ab2e636c3c79680942cdc73689d826
Matthew Gerring <matthewgerring@riseup.net>
2014-10-12 17:13:47 -0700

    util links fixed

--- wp-content/themes/digitalcitizen/socialbar.php
+++ wp-content/themes/digitalcitizen/socialbar.php
@@ -1,13 +1,16 @@
 <aside id="social-bar">
-	<span class="share-text">Share this article:</span>
-	<a href="" class="twitter-share icon-twitter">Twitter</a>
-	<a href="" class="facebook-share icon-facebook">Facebook</a>
+	<div id="share-links">
+		<span class="share-text">Share this article:</span>
+		<a href="" class="share twitter-share icon-twitter">Twitter</a>
+		<a href="" class="share facebook-share icon-facebook">Facebook</a>
+	</div>
+
 	<?php 
 	printf(
-		__( '<a href="%1$s" class="icon-link" rel="bookmark">Permalink</a>', 'digitalcitizen' ),
+		__( '<a href="%1$s" class="icon-link permalink" rel="bookmark">Permalink</a>', 'digitalcitizen' ),
 		get_permalink()
 	);
 	?>
-	<?php edit_post_link( __( 'Edit', 'digitalcitizen' ), '<span class="edit-link">', '</span>' ); ?>
+	<?php edit_post_link( __( 'Edit', 'digitalcitizen' ) ); ?>
 
 </aside>
