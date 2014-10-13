17331c60d049cb74893d9016aa6e026bd9a92690
Matthew Gerring <matthewgerring@riseup.net>
2014-10-13 16:57:15 -0700

    positioned post navigation links correctly

--- wp-content/themes/digitalcitizen/inc/template-tags.php
+++ wp-content/themes/digitalcitizen/inc/template-tags.php
@@ -22,11 +22,11 @@ function digitalcitizen_paging_nav() {
 		<div class="nav-links">
 
 			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'digitalcitizen' ) ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( __( 'Older posts', 'digitalcitizen' ) ); ?></div>
 			<?php endif; ?>
 
 			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'digitalcitizen' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts', 'digitalcitizen' ) ); ?></div>
 			<?php endif; ?>
 
 		</div><!-- .nav-links -->
@@ -52,8 +52,8 @@ function digitalcitizen_post_nav() {
 		<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'digitalcitizen' ); ?></h1>
 		<div class="nav-links">
 			<?php
-				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', 'digitalcitizen' ) );
-				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link',     'digitalcitizen' ) );
+				previous_post_link( '<div class="nav-previous">%link</div>', _x( '%title', 'Previous post link', 'digitalcitizen' ) );
+				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title', 'Next post link',     'digitalcitizen' ) );
 			?>
 		</div><!-- .nav-links -->
 	</nav><!-- .navigation -->
