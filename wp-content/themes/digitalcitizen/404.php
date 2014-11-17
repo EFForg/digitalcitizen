dbbbf9489c704a3417a75560b60ff8ec4ca88818
Matthew Gerring <matthewgerring@riseup.net>
2014-11-16 19:55:37 -0800

    404 page cleanup

--- wp-content/themes/digitalcitizen/404.php
+++ wp-content/themes/digitalcitizen/404.php
@@ -11,11 +11,11 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'digitalcitizen' ); ?></h1>
+				<header class="entry-header">
+					<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'digitalcitizen' ); ?></h1>
 				</header><!-- .page-header -->
 
-				<div class="page-content">
+				<div class="entry-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'digitalcitizen' ); ?></p>
 
 					<?php get_search_form(); ?>
